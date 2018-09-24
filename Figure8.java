3df56515840be7fa15872da4cb56a9a7ed1cea5b
21867 <43053741+21867@users.noreply.github.com>
2018-09-23 17:27:00 -0700

    Add files via upload

---
+++ Figure8.java
@@ -0,0 +1,62 @@
+/**
+   @Author:
+      Date: Sep 07, 2006
+   Teacher:
+       Lab:
+      Misc:
+  */
+
+import kareltherobot.*;
+
+public class Figure8 extends UrRobot
+{   
+    public Figure8(int s, int a, Direction dir, int beeps)
+    {
+        super(3, 5, North, 0);
+    }
+    public void task()  {
+           
+        super.turnLeft();
+        super.move();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnLeft();
+        super.move();
+        super.move();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnLeft();
+        super.move();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnLeft();
+        super.turnOff();
+        //add your code here
+            
+           
+   
+        } 
+
+}
